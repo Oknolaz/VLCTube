83f83b4686b9fea303c88543ca21541d8311a32e
Oknolaz <oknolaz.freedom@protonmail.com>
2020-12-30 15:22:31 +0300

    Add files via upload

---
+++ design.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'vlc.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(727, 519)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.back = QtWidgets.QPushButton(self.centralwidget)
+        self.back.setText("")
+        icon = QtGui.QIcon.fromTheme("back")
+        self.back.setIcon(icon)
+        self.back.setObjectName("back")
+        self.horizontalLayout.addWidget(self.back)
+        self.forward = QtWidgets.QPushButton(self.centralwidget)
+        self.forward.setText("")
+        icon = QtGui.QIcon.fromTheme("next")
+        self.forward.setIcon(icon)
+        self.forward.setObjectName("forward")
+        self.horizontalLayout.addWidget(self.forward)
+        self.home = QtWidgets.QPushButton(self.centralwidget)
+        self.home.setText("")
+        icon = QtGui.QIcon.fromTheme("user-home")
+        self.home.setIcon(icon)
+        self.home.setObjectName("home")
+        self.horizontalLayout.addWidget(self.home)
+        self.reload = QtWidgets.QPushButton(self.centralwidget)
+        self.reload.setText("")
+        icon = QtGui.QIcon.fromTheme("reload")
+        self.reload.setIcon(icon)
+        self.reload.setObjectName("reload")
+        self.horizontalLayout.addWidget(self.reload)
+        spacerItem = QtWidgets.QSpacerItem(468, 18, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.settings = QtWidgets.QPushButton(self.centralwidget)
+        self.settings.setText("")
+        icon = QtGui.QIcon.fromTheme("settings")
+        self.settings.setIcon(icon)
+        self.settings.setObjectName("settings")
+        self.horizontalLayout.addWidget(self.settings)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.youtubeView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        self.youtubeView.setUrl(QtCore.QUrl("https://www.youtube.com/"))
+        self.youtubeView.setObjectName("youtubeView")
+        self.verticalLayout.addWidget(self.youtubeView)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setTextVisible(False)
+        self.progressBar.setObjectName("progressBar")
+        self.verticalLayout.addWidget(self.progressBar)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "VLCTube"))
+from PyQt5 import QtWebEngineWidgets
---
+++ vlctube.py
@@ -0,0 +1,51 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+import design
+from shell import shell
+
+class MainWindow(design.Ui_MainWindow, QtWidgets.QMainWindow, QtCore.QUrl):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.progressBar.hide()
+        self.youtubeView.setFocus()
+        self.youtubeView.loadStarted.connect(self.LoadProgress)
+        self.back.clicked.connect(self.backPage)
+        self.forward.clicked.connect(self.forwardPage)
+        self.home.clicked.connect(self.goHome)
+        self.reload.clicked.connect(self.reloadPage)
+        self.youtubeView.urlChanged.connect(self.tryToOpenVLC)
+        
+    def LoadProgress(self):
+        self.progressBar.show()
+        self.youtubeView.loadProgress.connect(self.progressBar.setValue)
+        self.youtubeView.loadFinished.connect(self.progressBar.hide)
+
+    def backPage(self):
+        self.youtubeView.back()
+        
+    def forwardPage(self):
+        self.youtubeView.forward()
+        
+    def goHome(self):
+        self.youtubeView.setUrl(QtCore.QUrl("https://youtube.com/"))
+        
+    def reloadPage(self):
+        self.youtubeView.reload()
+        
+    def tryToOpenVLC(self):
+        pageUrl = str(self.youtubeView.url())
+        pageUrl = pageUrl.replace('PyQt5.QtCore.QUrl', '').replace('(', '').replace("'", '').replace(")", '')
+        if "youtube.com" not in pageUrl:
+            self.youtubeView.setUrl(QtCore.QUrl("https://youtube.com/"))
+        elif "youtube.com/watch?v=" in pageUrl:
+            shell(f"vlc '{pageUrl}'")
+            self.youtubeView.back()
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+my_mainWindow = MainWindow()
+my_mainWindow.show()
+
+sys.exit(app.exec_())
